0f4879244b6c25e7150f5a149337f3ef205edd2d
Africk-Hunter <hunterafrick@gmail.com>
2024-10-16 22:45:34 -0700

    Email Form Update

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,15 +6,18 @@ const Contact = () => {
     <section id="Contact" className="contact">
         <section className='contactTaglinesWrapper'>
             <h2 className='contactTagline'>Want to get in touch?</h2>
-            <h1 className='contactSecondaryTagline'>You can contact me directly at <span class="underlined">HunterAfrick@gmail.com</span> or through this form</h1>
+            <h1 className='contactSecondaryTagline'>You can contact me directly at <span class="underlined" id='myEmailAddress'>HunterAfrick@gmail.com</span> or through this form</h1>
         </section>
 
         <section className='emailOptionsWrapper'>
             <div className='userEmailContainer'>
-                <input className='emailInput' placeholder='Your Email'></input>
+                <input className='emailInput' placeholder='Your Name' id='nameInput'></input>
+            </div>
+            <div className='userEmailContainer'>
+                <input className='emailInput' placeholder='Your Email Address' id='emailInput'></input>
             </div>
             <div className='userMessageContainer'>
-                <textarea className='messageInput' placeholder='Your Message'></textarea>
+                <textarea className='messageInput' placeholder='Your Message' id='messageInput'></textarea>
             </div>
             <div className='buttonWrapper'>
                 <button className='messageSubmitButton'>Submit<img className='submitImg' src={plane} alt=''></img></button>
