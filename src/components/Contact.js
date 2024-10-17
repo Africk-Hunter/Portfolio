50db78f9af47335f18633ea4eb2ab60dfc463072
Africk-Hunter <hunterafrick@gmail.com>
2024-10-17 11:11:15 -0700

    Email System fix

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,25 +2,16 @@ import React, { useState } from 'react';
 import plane from '../images/planeIcon.svg';
 
 const Contact = () => {
-  const [formData, setFormData] = useState({
-    name: '',
-    email: '',
-    message: '',
-  });
-
+  const [formData, setFormData] = useState({ name: '', email: '', message: '' });
   const [submitted, setSubmitted] = useState(false);
 
   const handleChange = (e) => {
     const { id, value } = e.target;
-    setFormData((prevState) => ({
-      ...prevState,
-      [id]: value,
-    }));
+    setFormData((prevState) => ({ ...prevState, [id]: value }));
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    
     const form = e.target;
     fetch('/', {
       method: 'POST',
@@ -35,54 +26,31 @@ const Contact = () => {
     <section id="Contact" className="contact">
       <section className="contactTaglinesWrapper">
         <h2 className="contactTagline">Want to get in touch?</h2>
-        <h1 className="contactSecondaryTagline">
-          You can contact me directly at <span className="underlined" id="myEmailAddress">HunterAfrick@gmail.com</span> or through this form
-        </h1>
+        <h1 className="contactSecondaryTagline">You can contact me directly at <span className="underlined" id="myEmailAddress">HunterAfrick@gmail.com</span> or through this form</h1>
       </section>
 
       <section className="emailOptionsWrapper">
         {submitted ? (
           <p>Thanks for your message! I'll get back to you soon.</p>
         ) : (
-          <form className="emailOptionsWrapper" onSubmit={handleSubmit} name="contact" method="POST" data-netlify="true" netlify-honeypot="bot-field">
+          <form className='emailOptionsWrapper' onSubmit={handleSubmit} name="contact" method="POST" data-netlify="true" netlify-honeypot="bot-field">
             <input type="hidden" name="form-name" value="contact" />
             <input type="hidden" name="bot-field" />
 
             <div className="userEmailContainer">
-              <input
-                className="emailInput"
-                placeholder="Your Name"
-                id="name"
-                value={formData.name}
-                onChange={handleChange}
-                required
-              />
+              <input className="emailInput" name="name" id="name" placeholder="Your Name" value={formData.name} onChange={handleChange} required />
             </div>
+
             <div className="userEmailContainer">
-              <input
-                className="emailInput"
-                placeholder="Your Email Address"
-                id="email"
-                value={formData.email}
-                onChange={handleChange}
-                required
-                type="email"
-              />
+              <input className="emailInput" name="email" id="email" placeholder="Your Email Address" value={formData.email} onChange={handleChange} required type="email" />
             </div>
+
             <div className="userMessageContainer">
-              <textarea
-                className="messageInput"
-                placeholder="Your Message"
-                id="message"
-                value={formData.message}
-                onChange={handleChange}
-                required
-              />
+              <textarea className="messageInput" name="message" id="message" placeholder="Your Message" value={formData.message} onChange={handleChange} required />
             </div>
+
             <div className="buttonWrapper">
-              <button className="messageSubmitButton" type="submit">
-                Submit <img className="submitImg" src={plane} alt="" />
-              </button>
+              <button className="messageSubmitButton" type="submit">Submit <img className="submitImg" src={plane} alt="" /></button>
             </div>
           </form>
         )}
